fb0808803478259dfa2c7269fe9a99362188c6c9
mikael <mikael.ahola@skillio.ai>
2024-11-06 14:16:56 +0200

    Changed SQL query

--- src/data/Report_software.py
+++ src/data/Report_software.py
@@ -20,12 +20,9 @@ def fetch_time_tracking_data(conn, start_date, end_date):
     
     query = """
     SELECT consultant_name, customer_name,
-       SUM(
-           EXTRACT(EPOCH FROM (end_time - start_time - lunch_break)) / 3600
-       ) AS total_seconds
+       EXTRACT(EPOCH FROM (end_time - start_time - lunch_break)) / 3600 AS total_hours
     FROM time_tracking
-    WHERE start_time BETWEEN %s AND %s
-    GROUP BY consultant_name, customer_name;
+    WHERE start_time BETWEEN %s AND %s;
     """
     
     cursor.execute(query, (start_date, end_date))
